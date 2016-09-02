c24df55bd1d547b3aec9493abc96d08e600699bc
Henrik <henrik_mathiesen@hotmail.com>
2016-09-02 17:47:35 +0200

    -labbing with classes

--- app/js/TestClassCreation.js
+++ app/js/TestClassCreation.js
@@ -1,3 +1,11 @@
 export default class TestClassCreation {
 
+    constructor(id, name){
+        console.log("In TestClassCreation constructor " + "id: " + id + " name: " + name);
+        
+        // These are now attached to the instance
+        this.id = id;
+        this.name = name;
+    }
+
 } 
--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 import TestClassCreation from './TestClassCreation';
 
-let testClassCreation = new TestClassCreation();
+let testClassCreation = new TestClassCreation("123", "Henry");
 
 console.log(typeof TestClassCreation);                                  // function
 console.log(typeof testClassCreation);                                  // object
 console.log(testClassCreation instanceof TestClassCreation);            // true
+console.log(testClassCreation.id);                                      // 123
+console.log(testClassCreation.name);                                    // Henry
