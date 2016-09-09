787112208466d659678a38ebc5934245795fe25a
Henrik <henrik_mathiesen@hotmail.com>
2016-09-09 17:19:35 +0200

    -data -dataService

---
+++ app/js/services/FleetDataService.js
@@ -0,0 +1,11 @@
+import Car from '../Car';
+import Drone from '../Drone';
+
+export default class FleetDataService {
+
+    constructor() {
+        this.cars = [];
+        this.drones = [];
+    }
+
+}
---
+++ app/js/services/fleetData.js
@@ -0,0 +1,30 @@
+export let fleet = [
+    {
+        license: 'ABC123',
+        type: 'drone',
+        model: 'Amazon 1250',
+        airTimeHours: '6050',
+        base: 'New York'
+    },
+    {
+        license: 'XYZ975',
+        type: 'drone',
+        model: 'Markurius 22',
+        airTimeHours: '1120',
+        base: 'Chicago'
+    },
+    {
+        license: 'ABF202',
+        type: 'car',
+        model: 'Volvo V70',
+        miles: '1135',
+        base: 'New York'
+    },
+    {
+        license: 'ABY100',
+        type: 'car',
+        model: 'Mercedes',
+        miles: '5529',
+        base: 'Madrid'
+    }
+];
