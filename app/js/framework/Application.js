a1a8edf5f23195fb056d9bdabe8f97c9822072eb
Henrik <henrik_mathiesen@hotmail.com>
2016-09-14 20:07:00 +0200

    -application classes

---
+++ app/js/framework/Application.js
@@ -0,0 +1,7 @@
+import ApplicationBase from './ApplicationBase';
+
+export default class Application  extends ApplicationBase {
+    constructor() {
+        super();
+    }
+}
---
+++ app/js/framework/ApplicationBase.js
@@ -0,0 +1,5 @@
+export default class ApplicationBase {
+    constructor(title) {
+        this.title = title;
+    }
+}
